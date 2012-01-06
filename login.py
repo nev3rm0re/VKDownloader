36ec6276fd41a60d16ab2c71407f6b85b1377d6a
German Rumm <german.rumm@gmail.com>
2012-01-06 19:27:54 +0200

    Refactoring
    Split getAudio() logic into several methods, renamed it according to PEP8

--- login.py
+++ login.py
@@ -8,7 +8,9 @@ import ConfigParser
 
 import string
 import unicodedata
+
 from  download import *
+from config import *
 
 import HTMLParser
 
@@ -62,7 +64,56 @@ class WebGamePlayer(object):
         response = self.opener.open("https://login.vk.com/?act=login", login_data)
         return ''.join(response.readlines())
     
-    def getAudio(self):
+    def get_song_list(self, json, album=None):
+        urls = []
+        h = HTMLParser.HTMLParser()
+        for a in json:
+            filename = cleanUpForFilename(h.unescape(" - ".join(a[5:7])))
+            if (album == None or a[8] == album):
+                if len(filename) > 100:
+                    filename = filename[:125]
+                urls.append((a[2], filename + ".mp3"))
+        return urls
+    
+    def extract_songs(self, response_splitted):
+        song_list_json = fix_vk_json(response_splitted)
+        songs_json    = json.loads(song_list_json)
+        
+        urls = []
+        urls_by_id = {}
+        urls_by_album = {}
+        
+        h = HTMLParser.HTMLParser()
+        for a in songs_json["all"]:
+            filename = cleanUpForFilename(h.unescape(" - ".join(a[5:7])))
+            if len(filename) > 100:
+                filename = filename[:125]
+                
+            song_id = a[1]
+            song_url = a[2]
+            album_id = a[8]
+                
+            song_data = (song_url, filename + ".mp3")
+            
+            urls.append(song_data)
+            
+            urls_by_id[song_id] = song_data 
+            
+            if album_id in urls_by_album:
+                urls_by_album[album_id] += song_id
+            else:
+                urls_by_album[album_id] = [song_id]
+                
+        self.urls = urls
+        self.urls_by_album = urls_by_album
+        self.urls_by_id = urls_by_id
+    
+    def get_album_list(self, response_splitted):
+        json_decoded = json.loads(fix_vk_json(response_splitted))
+        albums = json_decoded["albums"].values()
+        return albums
+    
+    def request_audio(self):
         audio_url = "http://vkontakte.ru/audio"
         
         # extract user_id from response
@@ -80,18 +131,29 @@ class WebGamePlayer(object):
           'gid' : '0',
           'id'  : user_id})
         
-        
         response = self.opener.open(audio_url, post_data)
         
+        return response
+    
+    def process_response(self, response):
         response_html   = ''.join(response.readlines())
         response_html   = response_html.decode('windows-1251');
         response_splitted = response_html.split('<!>')
-        response_html = response_splitted[-2]
         
-        json_decoded = json.loads(fix_vk_json(response_splitted[-1]))
-        albums = json_decoded["albums"].values()
-            
+        self.albums = self.get_album_list(response_splitted[-1])
+        self.extract_songs(response_splitted[-2])
+    
+    def get_audio(self):
+        response = self.request_audio()
+        
+        self.process_response(response)
+        
+        # Extracting albums first
+        albums = self.albums
+        
         index = 0
+        
+        print "keys: ", self.urls_by_album.keys()
         
         print "[%d] [%s]" % (1, "*all*")
         
@@ -103,35 +165,26 @@ class WebGamePlayer(object):
         if selected_album == 1:
             album_id = None
         else:
-            album_id = albums[int(selected_album)-2]['id']
+            album_id = albums[int(selected_album) - 2]['id']
             
-        response_html = fix_vk_json(response_html)
+        if album_id is not None:
+            urls = []
+            for id in self.urls_by_album[album_id]:
+                urls.append(self.urls_by_id[id])
+        else:
+            urls = self.urls 
         
-        json_decoded    = json.loads(response_html)
-        h = HTMLParser.HTMLParser()
-        
-        urls = []
-        
-        for a in json_decoded["all"]:
-            filename = cleanUpForFilename(h.unescape(" - ".join(a[5:7])))
-            if (album_id == None or a[8] == album_id):
-                if len(filename) > 100:
-                    filename = filename[:125]
-                urls.append((a[2], filename + ".mp3"))
-            
         downloader = Downloader(urls)
-        print urls
         downloader.startDownload()
         
         
 config = ConfigParser.ConfigParser()
 read_status = config.read("config.ini")
 if len(read_status) == 0:
-    from config import *
     (vk_username, vk_password) = ask_for_login()
 else: 
     vk_username = config.get('login details', 'username')
     vk_password = config.get('login details', 'password')
 
 f = WebGamePlayer(vk_username,vk_password)
-f.getAudio()
+f.get_audio()
